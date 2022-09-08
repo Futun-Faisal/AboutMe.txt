1d7aa14f3f66199f6a9e30c6df60703c11c3019e
fton4 <s443005830@st.uqu.edu.sa>
2022-09-08 03:19:24 +0300

    my first commit

---
+++ src/src/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package src;
+
+/**
+ *
+ * @author areej
+ */
+public class Main{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+        System.out.println("Hi my name is Futun Faisal ");
+    }
+
+}
